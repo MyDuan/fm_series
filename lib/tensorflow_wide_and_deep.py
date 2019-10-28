38352b6d5b5ac5d8ecce89da0fa2745ac57e6979
MyDuan <dy1226130575@163.com>
2019-10-28 12:25:02 +0900

    Refactoring: Abolish the tf.contrib which are removed by tensorflow in 2.0.

--- lib/tensorflow_wide_and_deep.py
+++ lib/tensorflow_wide_and_deep.py
@@ -33,47 +33,48 @@ def eval_input_fn():
 
 
 def model():
-    gender = tf.contrib.layers.sparse_column_with_keys(column_name="gender", keys=["Female", "Male"])
-    race = tf.contrib.layers.sparse_column_with_keys(column_name="race", keys=["Amer-Indian-Eskimo", "Asian-Pac-Islander", "Black", "Other",
+    gender = tf.feature_column.categorical_column_with_vocabulary_list(key="gender", vocabulary_list=["Female", "Male"])
+    race = tf.feature_column.categorical_column_with_vocabulary_list(key="race", vocabulary_list=["Amer-Indian-Eskimo", "Asian-Pac-Islander", "Black", "Other",
                                                        "White"])
-    education = tf.contrib.layers.sparse_column_with_hash_bucket("education", hash_bucket_size=_HASH_BUCKET_SIZE)
-    relationship = tf.contrib.layers.sparse_column_with_hash_bucket("relationship", hash_bucket_size=_HASH_BUCKET_SIZE)
-    workclass = tf.contrib.layers.sparse_column_with_hash_bucket("workclass", hash_bucket_size=_HASH_BUCKET_SIZE)
-    occupation = tf.contrib.layers.sparse_column_with_hash_bucket("occupation", hash_bucket_size=_HASH_BUCKET_SIZE)
-    native_country = tf.contrib.layers.sparse_column_with_hash_bucket("native_country", hash_bucket_size=_HASH_BUCKET_SIZE)
+    education = tf.feature_column.categorical_column_with_hash_bucket("education", hash_bucket_size=_HASH_BUCKET_SIZE)
+    relationship = tf.feature_column.categorical_column_with_hash_bucket("relationship", hash_bucket_size=_HASH_BUCKET_SIZE)
+    workclass = tf.feature_column.categorical_column_with_hash_bucket("workclass", hash_bucket_size=_HASH_BUCKET_SIZE)
+    occupation = tf.feature_column.categorical_column_with_hash_bucket("occupation", hash_bucket_size=_HASH_BUCKET_SIZE)
+    native_country = tf.feature_column.categorical_column_with_hash_bucket("native_country", hash_bucket_size=_HASH_BUCKET_SIZE)
 
-    age = tf.contrib.layers.real_valued_column("age")
-    age_buckets = tf.contrib.layers.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
-    education_num = tf.contrib.layers.real_valued_column("education_num")
-    capital_gain = tf.contrib.layers.real_valued_column("capital_gain")
-    capital_loss = tf.contrib.layers.real_valued_column("capital_loss")
-    hours_per_week = tf.contrib.layers.real_valued_column("hours_per_week")
+    age = tf.feature_column.numeric_column("age")
+    age_buckets = tf.feature_column.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+    education_num = tf.feature_column.numeric_column("education_num")
+    capital_gain = tf.feature_column.numeric_column("capital_gain")
+    capital_loss = tf.feature_column.numeric_column("capital_loss")
+    hours_per_week = tf.feature_column.numeric_column("hours_per_week")
 
     base_columns = [
         gender, race, native_country, education, occupation, workclass, relationship, age_buckets
     ]
 
     crossed_columns = [
-        tf.contrib.layers.crossed_column([education, occupation], hash_bucket_size=_HASH_BUCKET_SIZE),
-        tf.contrib.layers.crossed_column([native_country, occupation], hash_bucket_size=_HASH_BUCKET_SIZE),
-        tf.contrib.layers.crossed_column([age_buckets, education, occupation], hash_bucket_size=_HASH_BUCKET_SIZE)
+        tf.feature_column.crossed_column(['education', 'occupation'], hash_bucket_size=_HASH_BUCKET_SIZE),
+        tf.feature_column.crossed_column(['native_country', 'occupation'], hash_bucket_size=_HASH_BUCKET_SIZE),
+        tf.feature_column.crossed_column([age_buckets, 'education', 'occupation'], hash_bucket_size=_HASH_BUCKET_SIZE)
     ]
 
     wide_columns = base_columns + crossed_columns
 
     deep_columns = [
-        tf.contrib.layers.embedding_column(workclass, dimension=8),
-        tf.contrib.layers.embedding_column(education, dimension=8),
-        tf.contrib.layers.embedding_column(gender, dimension=8),
-        tf.contrib.layers.embedding_column(relationship, dimension=8),
-        tf.contrib.layers.embedding_column(native_country, dimension=8),
-        tf.contrib.layers.embedding_column(occupation, dimension=8),
+        tf.feature_column.embedding_column(workclass, dimension=8),
+        tf.feature_column.embedding_column(education, dimension=8),
+        tf.feature_column.embedding_column(gender, dimension=8),
+        tf.feature_column.embedding_column(relationship, dimension=8),
+        tf.feature_column.embedding_column(native_country, dimension=8),
+        tf.feature_column.embedding_column(occupation, dimension=8),
         age, education_num, capital_gain, capital_loss, hours_per_week]
 
     model_dir = "./model"
-    model = tf.contrib.learn.DNNLinearCombinedClassifier(
+    estimator = tf.estimator.DNNLinearCombinedClassifier(
         model_dir=model_dir,
         linear_feature_columns=wide_columns,
         dnn_feature_columns=deep_columns,
-        dnn_hidden_units=[100, 50])
-    return model
+        dnn_hidden_units=[100, 50]
+    )
+    return estimator
